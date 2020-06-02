f7bbedefd83dc326251fb5ad2052b9aa7bde8a73
Ognjen <ognjengari@gmail.com>
2020-06-02 22:57:20 +0200

    added person domain

---
+++ backend/src/main/java/com/ognjengaric/demo/domain/Person.java
@@ -0,0 +1,65 @@
+package com.ognjengaric.demo.domain;
+
+import javax.persistence.*;
+
+@Table
+public class Person {
+
+    @Id
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String password;
+
+    @Column
+    private String image;
+
+    public Person() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
