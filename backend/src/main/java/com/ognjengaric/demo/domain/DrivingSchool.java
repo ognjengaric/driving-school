8f9d9f3d0a07406b4e900fbf0cf8aae536878537
Ognjen <ognjengari@gmail.com>
2020-06-03 00:15:32 +0200

    added driving school repository

--- backend/src/main/java/com/ognjengaric/demo/domain/DrivingSchool.java
+++ backend/src/main/java/com/ognjengaric/demo/domain/DrivingSchool.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 
 import com.ognjengaric.demo.enums.CategoryType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Table
@@ -24,5 +25,39 @@ public class DrivingSchool {
     @ElementCollection
     private List<CategoryType> availableCategories;
 
+    public DrivingSchool() {
+        this.availableCategories = new ArrayList<>();
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public List<CategoryType> getAvailableCategories() {
+        return availableCategories;
+    }
+
+    public void setAvailableCategories(List<CategoryType> availableCategories) {
+        this.availableCategories = availableCategories;
+    }
 }
