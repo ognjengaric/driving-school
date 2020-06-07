5040ba632e3e6cc04d7afa695094b9a72983207c
Ognjen <ognjengari@gmail.com>
2020-06-07 16:05:14 +0200

    initial route object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Router } from 'react-router';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes/>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+const Routes = () => {
+
+    return(
+        <Switch>
+        </Switch>
+    );
+}
+
+
+export default Routes;
