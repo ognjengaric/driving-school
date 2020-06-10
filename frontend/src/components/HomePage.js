291e50cd5950e3c79fb101b7bf81232c7dce0596
Ognjen <ognjengari@gmail.com>
2020-06-10 20:45:32 +0200

    frontend login logic added

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import {makeStyles} from '@material-ui/styles';
+import {useLocation} from 'react-router'
+import {Alert} from '@material-ui/lab';
+import {Snackbar} from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +16,24 @@ const useStyles = makeStyles((theme) => ({
 const HomePage = () => {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [show, setShow] = useState(false);
+
+    let location = useLocation();
+
+    useEffect(() => {
+      console.log(location)
+      if(location.state)
+        if(location.state.prev === 'login')
+          setShow(true)
+    }, [location]);
+
+    const alert = () => (
+      <Snackbar open={show} autoHideDuration={5000} onClose={() => setShow(false)} anchorOrigin={{vertical:"top", horizontal:"right"}}>
+          <Alert onClose={() => setShow(false)} severity="success" variant="filled">
+              Successfull login!
+          </Alert>
+      </Snackbar>
+  )
 
     const handleChange = (event, value) => {        
       setOpen(value);
@@ -22,6 +43,7 @@ const HomePage = () => {
         <div className={classes.root}>
             <Header handleChange={handleChange}/>
             <Sidebar open={open} handleChange={handleChange}/>
+            {alert()}
         </div>
     );
 } 
